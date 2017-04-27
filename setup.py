56e7046d6144f0109163be44458e3f7e853129c8
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-04-27 19:20:46 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name='Niamoto',
+    version='0.1',
+    description='Niamoto core application',
+    author='Dimitri Justeau',
+    author_email='dimitri.justeau@gmail.com',
+    url='https://github.com/dimitri-justeau/niamoto-core/',
+    packages=['niamoto'],
+ )
