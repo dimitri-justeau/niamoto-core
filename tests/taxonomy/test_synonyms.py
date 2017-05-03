d774ca10f0155c996108157e08fa709977dd27f6
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-05-03 14:05:10 +0200

    Test duplicate synonyms

--- tests/taxonomy/test_synonyms.py
+++ tests/taxonomy/test_synonyms.py
@@ -2,6 +2,8 @@
 
 import unittest
 
+from sqlalchemy.exc import IntegrityError
+
 from niamoto.taxonomy.taxon import Taxon
 from niamoto.db.connector import Connector
 from niamoto.db import metadata as niamoto_db_meta
@@ -26,13 +28,21 @@ class TestGetTaxon(BaseTestNiamotoSchemaCreated):
             'test_data_provider_1',
             database=settings.TEST_DATABASE,
         )
+        TestDataProvider.register_data_provider(
+            'test_data_provider_2',
+            database=settings.TEST_DATABASE,
+        )
 
     def tearDown(self):
         Taxon.delete_all_taxa(database=settings.TEST_DATABASE)
 
-    def test_get_not_empty_raw_taxon_dataset(self):
+    def test_add_single_synonym(self):
         data_provider_1 = TestDataProvider(
             'test_data_provider_1',
+            database=settings.TEST_DATABASE,
+        )
+        data_provider_2 = TestDataProvider(
+            'test_data_provider_2',
             database=settings.TEST_DATABASE,
         )
         data = [
@@ -61,7 +71,73 @@ class TestGetTaxon(BaseTestNiamotoSchemaCreated):
         df1 = Taxon.get_raw_taxon_dataset(
             database=settings.TEST_DATABASE
         )
-        print(df1)
+        self.assertEqual(
+            df1.loc[0]['synonyms'],
+            {data_provider_1.get_type_name(): 1}
+        )
+        Taxon.add_synonym_for_single_taxon(
+            0,
+            data_provider_2,
+            2,
+            database=settings.TEST_DATABASE
+        )
+        df2 = Taxon.get_raw_taxon_dataset(
+            database=settings.TEST_DATABASE
+        )
+        self.assertEqual(
+            df2.loc[0]['synonyms'],
+            {
+                data_provider_1.get_type_name(): 1,
+                data_provider_2.get_type_name(): 2,
+            }
+        )
+
+    def test_duplicate_synonym(self):
+        data_provider_1 = TestDataProvider(
+            'test_data_provider_1',
+            database=settings.TEST_DATABASE,
+        )
+        data = [
+            {
+                'id': 0,
+                'full_name': 'Family One',
+                'rank_name': 'One',
+                'rank': niamoto_db_meta.TaxonRankEnum.FAMILIA,
+                'parent_id': None,
+                'synonyms': {},
+                'mptt_left': 0,
+                'mptt_right': 0,
+                'mptt_tree_id': 0,
+                'mptt_depth': 0,
+            },
+            {
+                'id': 1,
+                'full_name': 'Family Two',
+                'rank_name': 'Two',
+                'rank': niamoto_db_meta.TaxonRankEnum.FAMILIA,
+                'parent_id': None,
+                'synonyms': {},
+                'mptt_left': 0,
+                'mptt_right': 0,
+                'mptt_tree_id': 0,
+                'mptt_depth': 0,
+            },
+        ]
+        ins = niamoto_db_meta.taxon.insert().values(data)
+        with Connector.get_connection(settings.TEST_DATABASE) as connection:
+            connection.execute(ins)
+        Taxon.add_synonym_for_single_taxon(
+            0,
+            data_provider_1,
+            1,
+            database=settings.TEST_DATABASE,
+        )
+        self.assertRaises(
+            IntegrityError,
+            Taxon.add_synonym_for_single_taxon,
+            1, data_provider_1, 1,
+            database=settings.TEST_DATABASE,
+        )
 
 if __name__ == '__main__':
     TestDatabaseManager.setup_test_database()
