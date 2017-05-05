5279ac03e848c0e123161ce4392632a4777d92f6
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-05-05 17:04:42 +0200

    Start implementing cli

---
+++ niamoto/bin/cli.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+import click
+
+
+@click.command()
+def niamoto_cli():
+    """
+    Niamoto command line interface entry point.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    niamoto_cli()
--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 # coding: utf-8
 
+import os
 from setuptools import find_packages, setup
 
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_dir, 'README.md'), encoding='utf-8') as file:
+    long_description = file.read()
 
 setup(
     name='Niamoto',
@@ -11,4 +16,9 @@ setup(
     author_email='dimitri.justeau@gmail.com',
     url='https://github.com/dimitri-justeau/niamoto-core/',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'niamoto=niamoto.bin.cli:niamoto_cli',
+        ],
+    },
  )
