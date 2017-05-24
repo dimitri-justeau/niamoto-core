a6a67ad5250b37002420ebbb53b643a775ecacf4
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-05-24 13:37:42 +0200

    Ensure NIAMOTO_HOME is added to path when set

--- niamoto/conf.py
+++ niamoto/conf.py
@@ -22,6 +22,8 @@ else:
 
 def set_niamoto_home(niamoto_home_path=None):
     global NIAMOTO_HOME
+    if NIAMOTO_HOME is sys.path:
+        sys.path.remove(NIAMOTO_HOME)
     if niamoto_home_path is None:
         if HOME_ENVIRONMENT_VARIABLE not in os.environ:
             NIAMOTO_HOME = DEFAULT_NIAMOTO_HOME
@@ -30,14 +32,13 @@ def set_niamoto_home(niamoto_home_path=None):
     else:
         NIAMOTO_HOME = niamoto_home_path
         os.environ[HOME_ENVIRONMENT_VARIABLE] = NIAMOTO_HOME
+    # Append NIAMOTO_HOME to sys.path
+    sys.path.append(NIAMOTO_HOME)
 
 
 # Create home folder if not existing
 if not os.path.exists(NIAMOTO_HOME):
     os.makedirs(NIAMOTO_HOME)
-
-# Append NIAMOTO_HOME to sys.path
-sys.path.append(NIAMOTO_HOME)
 
 # Global settings
 settings = None
