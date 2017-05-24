a47dacf7ef1f3dbd5911f2191dbb9ebdcf35ed46
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-05-24 19:23:26 +0200

    Use NIAMOTO_HOME environment variable in init_niamoto_home if available

--- niamoto/bin/commands/init_niamoto_home.py
+++ niamoto/bin/commands/init_niamoto_home.py
@@ -1,16 +1,23 @@
 # coding: utf-8
 
+import os
+
 import click
 
-from niamoto.constants import DEFAULT_NIAMOTO_HOME
+from niamoto.constants import DEFAULT_NIAMOTO_HOME, HOME_ENVIRONMENT_VARIABLE
 
 
 @click.command('init_niamoto_home')
-@click.option('--niamoto_home_path', default=DEFAULT_NIAMOTO_HOME)
-def init_niamoto_home_cli(niamoto_home_path=DEFAULT_NIAMOTO_HOME):
+@click.option('--niamoto_home_path', default=None)
+def init_niamoto_home_cli(niamoto_home_path=None):
     """
     Initialize the Niamoto home directory.
     """
+    if niamoto_home_path is None:
+        if HOME_ENVIRONMENT_VARIABLE in os.environ:
+            niamoto_home_path = os.environ[HOME_ENVIRONMENT_VARIABLE]
+        else:
+            niamoto_home_path = DEFAULT_NIAMOTO_HOME
     from niamoto.api import init_niamoto_home as api_init_niamoto_home
     click.echo("Initializing Niamoto home directory...")
     try:
