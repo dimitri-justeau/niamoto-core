997b7c74dab30c8e11c2902fe3160ef36deacfaf
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-06-15 15:28:10 +0200

    Implement and test RDataPublisher

--- niamoto/data_publishers/__init__.py
+++ niamoto/data_publishers/__init__.py
@@ -1,4 +1,10 @@
 # coding: utf-8
 
+import os
+
+from niamoto.conf import NIAMOTO_HOME
 from niamoto.data_publishers.occurrence_data_publisher import \
     OccurrenceDataPublisher
+
+
+R_SCRIPTS_HOME = os.path.join()
