b1db48fd89dae5a46a066613fd1142d058e2ec63
Dimitri Justeau <dimitri.justeau@gmail.com>
2017-07-18 21:01:03 +0200

    Implement vector dimension

---
+++ niamoto/data_marts/dimensions/vector_dimension.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+
+
+from niamoto.data_marts.dimensions.base_dimension import BaseDimension
+from niamoto.vector.vector_manager import VectorManager
+from niamoto.data_publishers.vector_publisher import VectorDataPublisher
+
+
+class VectorDimension(BaseDimension):
+    """
+    Dimension extracted from a registered vector layer.
+    """
+
+    def __init__(self, vector_name):
+        self.vector_name = vector_name
+        pk_cols = VectorManager.get_vector_primary_key_columns(vector_name)
+        self.geom_col = VectorManager.get_geometry_column(vector_name)
+        pk_cols = [i[0] for i in pk_cols]
+        tbl = VectorManager.get_vector_sqlalchemy_table(vector_name)
+        columns = [c.copy() for c in tbl.columns if c.name not in pk_cols]
+        super(VectorDimension, self).__init__(
+            vector_name,
+            columns,
+            publisher=VectorDataPublisher()
+        )
+
+    def populate_from_publisher(self, *args, **kwargs):
+        return super(VectorDimension, self).populate_from_publisher(
+            self.vector_name,
+            *args,
+            **kwargs
+        )
+
+    def populate(self, dataframe):
+        geom_col_name = self.geom_col[0]
+        srid = self.geom_col[2]
+        dataframe[geom_col_name] = dataframe[geom_col_name].apply(
+            lambda x: "SRID={};{}".format(srid, x)
+        )
+        return super(VectorDimension, self).populate(dataframe)
+
+    @classmethod
+    def get_description(cls):
+        return "Vector dimension."
+
+    @classmethod
+    def get_key(cls):
+        return "VECTOR_DIMENSION"
---
+++ tests/data_marts/test_vector_dimension.py
@@ -0,0 +1,74 @@
+# coding: utf-8
+
+import os
+import unittest
+
+from sqlalchemy.engine.reflection import Inspector
+
+from niamoto.testing import set_test_path
+
+set_test_path()
+
+from niamoto.conf import settings, NIAMOTO_HOME
+from niamoto.testing.test_database_manager import TestDatabaseManager
+from niamoto.testing.base_tests import BaseTestNiamotoSchemaCreated
+from niamoto.api.vector_api import add_vector
+from niamoto.data_marts.dimensions.vector_dimension import VectorDimension
+from niamoto.db.connector import Connector
+
+
+SHP_TEST = os.path.join(
+    NIAMOTO_HOME, 'data', 'vector', 'NCL_adm', 'NCL_adm1.shp'
+)
+
+
+class TestVectorDimension(BaseTestNiamotoSchemaCreated):
+    """
+    Test case for VectorDimension class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestVectorDimension, cls).setUpClass()
+        add_vector(SHP_TEST, 'ncl_adm1')
+
+    def setUp(self):
+        super(TestVectorDimension, self).setUp()
+        self.tearDown()
+
+    def tearDown(self):
+        with Connector.get_connection() as connection:
+            inspector = Inspector.from_engine(connection)
+            tables = inspector.get_table_names(
+                schema=settings.NIAMOTO_FACT_TABLES_SCHEMA
+            )
+            for tb in tables:
+                connection.execute("DROP TABLE {};".format(
+                    "{}.{}".format(settings.NIAMOTO_FACT_TABLES_SCHEMA, tb)
+                ))
+        with Connector.get_connection() as connection:
+            inspector = Inspector.from_engine(connection)
+            tables = inspector.get_table_names(
+                schema=settings.NIAMOTO_DIMENSIONS_SCHEMA
+            )
+            for tb in tables:
+                connection.execute("DROP TABLE {};".format(
+                    "{}.{}".format(settings.NIAMOTO_DIMENSIONS_SCHEMA, tb)
+                ))
+
+    def test_vector_dimension(self):
+        dim = VectorDimension('ncl_adm1')
+        dim.create_dimension()
+        dim.populate_from_publisher()
+        dim.get_values()
+
+
+if __name__ == '__main__':
+    TestDatabaseManager.setup_test_database()
+    TestDatabaseManager.create_schema(settings.NIAMOTO_SCHEMA)
+    TestDatabaseManager.create_schema(settings.NIAMOTO_RASTER_SCHEMA)
+    TestDatabaseManager.create_schema(settings.NIAMOTO_VECTOR_SCHEMA)
+    TestDatabaseManager.create_schema(settings.NIAMOTO_DIMENSIONS_SCHEMA)
+    TestDatabaseManager.create_schema(settings.NIAMOTO_FACT_TABLES_SCHEMA)
+    unittest.main(exit=False)
+    TestDatabaseManager.teardown_test_database()
