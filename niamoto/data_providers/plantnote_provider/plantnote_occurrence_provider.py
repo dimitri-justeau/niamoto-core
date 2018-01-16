4815e7cded54f8c3ebe84d957bc4531fc37bd456
Dimitri Justeau <dimitri.justeau@gmail.com>
2018-01-16 12:03:33 +1100

    Update Plantnote provider

--- niamoto/data_providers/plantnote_provider/plantnote_occurrence_provider.py
+++ niamoto/data_providers/plantnote_provider/plantnote_occurrence_provider.py
@@ -60,6 +60,8 @@ class PlantnoteOccurrenceProvider(BaseOccurrenceProvider):
                 func.coalesce(obs_table.c["hauteur"], None).label("height"),
                 func.coalesce(obs_table.c["nb_tiges"], None).label("stem_nb"),
                 func.coalesce(obs_table.c["statut"], None).label("status"),
+                func.coalesce(inv_table.c["X"], None).label("pos_X"),
+                func.coalesce(inv_table.c["Y"], None).label("pos_Y"),
                 obs_table.c["date_observation"],
             ]).select_from(
                 occ_table.outerjoin(
@@ -92,6 +94,8 @@ class PlantnoteOccurrenceProvider(BaseOccurrenceProvider):
                 "stem_nb",
                 "status",
                 "date_observation",
+                "pos_X",
+                "pos_Y",
             ]
             properties = df[property_cols].apply(
                 lambda x: x.to_json(force_ascii=False),
